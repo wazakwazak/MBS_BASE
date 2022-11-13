8fd85c5b3b8f9996ce91e8517c5d1388e9c7be88
Beomseok Moon <beomseok.moon94@gmail.com>
2022-11-13 21:37:26 +0900

    [Title] Add re-defined type header file [Details] - Add re-defined type header file

---
+++ mType.h
@@ -0,0 +1,20 @@
+#ifndef M_TYPE_HEADER
+#define M_TYPE_HEADER
+
+#include <stdio.h>
+#include <stdbool.h>
+
+typedef bool			mBool;
+typedef char			mChar;
+typedef short			mInt16;
+typedef int				mInt32;
+typedef long long		mInt64;
+typedef float			mFloat;
+typedef double			mDouble;
+
+typedef unsigned char		mUChar;
+typedef unsigned short		mUInt16;
+typedef unsigned int		mUInt32;
+typedef unsigned long long	mUInt64;
+
+#endif
